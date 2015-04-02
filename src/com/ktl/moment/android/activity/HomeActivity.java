6789da2970cebbf68f72bc9cb24ed1dfa853236f
kdf5000@163.com <kong19920213>
2015-04-02 09:21:51 +0800

    kongdefei:修改菜单切换为attach和detach模式

--- src/com/ktl/moment/android/activity/HomeActivity.java
+++ src/com/ktl/moment/android/activity/HomeActivity.java
@@ -20,7 +20,7 @@ public class HomeActivity extends BaseActivity {
 	private CustomMenu customMenu;// 菜单
 	private FragmentManager fragmentManager;// 管理器
 	private FragmentTransaction fragmentTransaction;// fragment事务
-	private String currentFgTag = "";//当前菜单的tag
+	private static String currentFgTag = "FindFragment";//当前菜单的tag
 
 	@Override
 	protected void onCreate(Bundle arg0) {
@@ -31,8 +31,9 @@ public class HomeActivity extends BaseActivity {
 		fragmentManager = getSupportFragmentManager();//获取fragment的管理器
 		
 		//初始为发现界面
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.home_content_container, new FindFragment()).commit();
+//		getSupportFragmentManager().beginTransaction()
+//				.replace(R.id.home_content_container, new FindFragment()).commit();
+		switchMenuByTag(currentFgTag);//设置默认的界面
 	}
 
 	/**
@@ -48,7 +49,7 @@ public class HomeActivity extends BaseActivity {
 				R.color.found_background_color));
 		customMenu = (CustomMenu) findViewById(R.id.id_menu);
 	}
-
+	
 	/**
 	 * 初始化一些事件
 	 */
@@ -59,34 +60,90 @@ public class HomeActivity extends BaseActivity {
 			@Override
 			public void OnClick(View v, int pos) {
 				// TODO Auto-generated method stub
+				String tag = "";
 				switch (pos) {
 				case C.menu.FRAGMENT_FIND_MENU_ID:
-					currentFgTag =  C.menu.FRAGMENT_FIND_TAG;
+					tag =  C.menu.FRAGMENT_FIND_TAG;
 					break;
 				case C.menu.FRAGMENT_DYNAMIC_MENU_ID:
-					currentFgTag = C.menu.FRAGMENT_DYNAMIC_TAG;
+					tag = C.menu.FRAGMENT_DYNAMIC_TAG;
 					break;
 				case C.menu.FRAGMENT_MOMENT_MENU_ID:
-					currentFgTag = C.menu.FRAGMENT_MOMENT_TAG;
+					tag = C.menu.FRAGMENT_MOMENT_TAG;
 					break;
 				case C.menu.FRAGMENT_ME_MENU_ID:
-					currentFgTag = C.menu.FRAGMENT_ME_TAG;
+					tag = C.menu.FRAGMENT_ME_TAG;
 					break;
 				}
-				switchMenuByTag(currentFgTag);
+				switchMenuByTag(tag);
 			}
 		});
 	}
-
+	private void setCurrentTag (String tag){
+		this.currentFgTag = tag;
+	}
+	/**
+	 * 通过tag切换标签
+	 * @param selectedTag
+	 */
 	private void switchMenuByTag(String selectedTag){
 		fragmentTransaction = fragmentManager.beginTransaction();
 		fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);//
 		if(selectedTag!=null  && !selectedTag.equals("")){
-			
+			detachFragment(getFragmentByTag(currentFgTag));//detach当前的fragment
 		}
-		
+		attachFragment(R.id.home_content_container,getFragmentByTag(selectedTag),selectedTag);
+		commitTransactions();//提交事务
+		setCurrentTag(selectedTag);//设置当前的tag
 	}
-	
+	/**
+	 * 提交fragmenttransaction
+	 */
+	private void commitTransactions(){
+		if (fragmentTransaction != null && !fragmentTransaction.isEmpty()) {
+		    fragmentTransaction.commitAllowingStateLoss();
+		    fragmentTransaction = null;
+		}
+	}
+	/**
+	 * attach fragment
+	 * @param layout
+	 * @param f
+	 * @param tag
+	 */
+	private void attachFragment(int layout,Fragment f,String tag){
+		if(f!=null){
+			if (fragmentTransaction == null) {
+	            fragmentTransaction = fragmentManager.beginTransaction();
+	            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	        }
+			if(f.isDetached()){
+				fragmentTransaction.attach(f);
+			}else if(!f.isAdded()){
+				fragmentTransaction.add(layout, f,tag);
+			}else{
+				//Nothing to do
+			}
+		}
+	}
+	/**
+	 * detach fragment
+	 * @param f
+	 */
+	private void detachFragment(Fragment f){
+		if(f!=null && !f.isDetached()){
+			if (fragmentTransaction == null) {
+	            fragmentTransaction = fragmentManager.beginTransaction();
+	            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	        }
+			fragmentTransaction.detach(f);
+		}
+	}
+	/**
+	 * 通过标签获取fragment
+	 * @param tag
+	 * @return
+	 */
 	private Fragment getFragmentByTag(String tag){
 		Fragment f = fragmentManager.findFragmentByTag(tag);
 		if(f==null){
