68579827f74bfe5b71855baf732b7330dc3c966c
kdf5000@163.com <kong19920213>
2015-04-17 13:47:27 +0800

    kongdefei:添加个人页面

--- src/com/ktl/moment/android/fragment/MeFragment.java
+++ src/com/ktl/moment/android/fragment/MeFragment.java
@@ -7,15 +7,27 @@ import android.view.ViewGroup;
 
 import com.ktl.moment.R;
 import com.ktl.moment.android.base.BaseFragment;
+import com.ktl.moment.android.component.CircleImageView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class MeFragment extends BaseFragment {
 	private static final String TAG = "MeFragment";
+
+	@ViewInject(R.id.me_user_avatar)
+	private CircleImageView userAvatar;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View view = inflater.inflate(R.layout.fragment_me, container, false);
-		
+		ViewUtils.inject(this, view);
+
+		ImageLoader.getInstance().displayImage(
+				"http://7sbpmg.com1.z0.glb.clouddn.com/1.jpg", userAvatar,
+				getDisplayImageOptions());
 		return view;
 	}
 }
